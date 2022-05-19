630823ccc4a800b22bc52c2feb21ebcb3542ecfe
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-19 22:13:56 +0600

    added getting single item inventory by query string

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -3,12 +3,17 @@ const router = express.Router()
 const Inventory = require('../model/Inventory')
 
 
-
+// get all inventory items
 router.get('/inventory', async (req, res) =>{
     const inventory = await Inventory.find()
     res.json({inventory})
 })
 
+// get single inventory item
+router.get('/inventory/item', async (req, res) =>{
+    const inventoryItem = await Inventory.findOne({_id: req.query.id})
+    res.json({inventoryItem})
+})
 
 
 module.exports = router;
