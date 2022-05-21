5ed656a903952eb9879ac07ecea98a36d264dec8
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-05-22 02:08:44 +0600

    fixed MongoDB url problem

--- DB/db.js
+++ DB/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.jnizw.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.jnizw.mongodb.net/Fruits-Warehouse-Management?retryWrites=true&w=majority`
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
